8567be0cfd366b1b92c9f656cd81b34c4599654e
shawn prasad <shawnprasad92@gmail.com>
2018-07-24 19:50:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
 if (name === "Susan") {
-  console.log("The name is coreect")
+  console.log("The name is correct")
 } else {
   console.log ("Thought it was Susan")
 }
